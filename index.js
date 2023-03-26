9d4289180fe5f66e05f32510a9385f3f814fb21d
Martha <martha.maina@student.moringaschool.com>
2023-03-26 12:42:08 +0300

    assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue, secondValue, belowHqStreet){
+  return 43 - 42
+} 
+const someValue = subtract(43, 42)
+console.log(someValue)
+
+const secondValue = subtract(50, 42)
+console.log(secondValue)
+
+const belowHqStreet = subtract(42, 34)
+console.log(belowHqStreet)
+
+
+
+function distanceFromHqInFeet(someValue){
+distanceFromHqInBlocks(someValue);
+  // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+  // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+
+  // the return value of distanceFromHqInBlocks can then be used to calculate feet
+}
+
+function distacnceTravelledInFeet(start, destination){
+  //returns the number of feet traveled
+}
+
+function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+}
+
